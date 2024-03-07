ada0ea9db233d52ca10c54869743c8ec0b30f76b
romain-jacob <romain-jacob@users.noreply.github.com>
2024-03-07 08:07:16 +0000

    jekyll build from Action a56198537715a59a4df9a9e422c43fa60840577a

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "A fresh look for HotCarbon",
+        "excerpt":"We are happy to present the redesigned HotCarbon website.  You will find here all updates and info for the workshop’s upcoming edition(s) and the papers and talks from the previous editions.  ","categories": [],
+        "tags": [],
+        "url": "/2023/11/10/new-website.html",
+        "teaser": null
+      },{
+        "title": "HotCarbon'24 CfP is out!",
+        "excerpt":"HotCarbon will be back in 2024! Submission deadline is May 8, 2024.  Check out the Call for Papers for details.  ","categories": ["news"],
+        "tags": [],
+        "url": "/news/2023/11/13/cfpOut.html",
+        "teaser": null
+      },{
+        "title": "Important updates on remote participation",
+        "excerpt":"See the Policies section of the CfP for important updates on remote participation to HotCarbon’24.  ","categories": [],
+        "tags": [],
+        "url": "/2024/02/11/remoteparticipation.html",
+        "teaser": null
+      }]
