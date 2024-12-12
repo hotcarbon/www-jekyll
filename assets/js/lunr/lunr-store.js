94a826b111114778fa5074b737a08850f01adddc
romain-jacob <romain-jacob@users.noreply.github.com>
2024-12-12 15:34:36 +0000

    jekyll build from Action 26df5058adbfd3839c2f2fcbfa529bf8afb015fb

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,55 @@
+var store = [{
+        "title": "A fresh look for HotCarbon",
+        "excerpt":"We are happy to present the redesigned HotCarbon website.  You will find here all updates and info for the workshop’s upcoming edition(s) and the papers and talks from the previous editions.  ","categories": [],
+        "tags": [],
+        "url": "/2023/11/10/new-website.html",
+        "teaser": null
+      },{
+        "title": "HotCarbon'24 CfP is out!",
+        "excerpt":"HotCarbon will be back in 2024! Submission deadline is May 8, 2024.  Check out the Call for Papers for details.  ","categories": ["news"],
+        "tags": [],
+        "url": "/news/2023/11/13/cfpOut.html",
+        "teaser": null
+      },{
+        "title": "Important updates on remote participation",
+        "excerpt":"See the Policies section of the CfP for important updates on remote participation to HotCarbon’24.  ","categories": [],
+        "tags": [],
+        "url": "/2024/02/11/remoteparticipation.html",
+        "teaser": null
+      },{
+        "title": "[NEW] Registration deadline",
+        "excerpt":"There is now a paper registration deadline, set to May 5, 2024 (AoE).  ","categories": [],
+        "tags": [],
+        "url": "/2024/04/13/linkupAndRegistrationDeadline.html",
+        "teaser": null
+      },{
+        "title": "Change in location",
+        "excerpt":"The workshop location is changed from Santa Clara to Santa Cruz, CA.  It will be held in UCSC Haybarn.  ","categories": [],
+        "tags": [],
+        "url": "/2024/04/14/locationChange.html",
+        "teaser": null
+      },{
+        "title": "Registration is now open!",
+        "excerpt":"The workshop registration is now open.  You can find further details about attending the event on the corresponding page.  ","categories": [],
+        "tags": [],
+        "url": "/2024/06/12/registration.html",
+        "teaser": null
+      },{
+        "title": "The program in online",
+        "excerpt":"The workshop program is online, along with all the final papers and the videos of the virtual presentations. Check them out!  ","categories": [],
+        "tags": [],
+        "url": "/2024/07/07/program.html",
+        "teaser": null
+      },{
+        "title": "Wrap up of HotCarbon'24",
+        "excerpt":"The 2024 edition of HotCarbon was a vibrant success! Thank you to all authors and participants for the great interaction.  Stay tuned for the 2025 edition!  ","categories": [],
+        "tags": [],
+        "url": "/2024/07/11/wrapUp.html",
+        "teaser": null
+      },{
+        "title": "Get ready for HotCarbon'25!",
+        "excerpt":"After the success of its third edition, HotCarbon will be back in 2025 with a 1.5 day program! (or even 2 days, if necessary). Like in previous editions, the workshop will be collocated with OSDI and take place on July 10 and 11 in Cambridge, MA, USA. We are delighted...","categories": [],
+        "tags": [],
+        "url": "/2024/12/12/2025on.html",
+        "teaser": null
+      }]
