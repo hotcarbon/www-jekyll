aa14f935f372806a56c42045e8fd418d408248ab
romain-jacob <romain-jacob@users.noreply.github.com>
2025-09-17 12:48:59 +0000

    jekyll build from Action c11bb33e480a959a21088b23a8edb62ed8c7c88b

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,103 @@
+var store = [{
+        "title": "A fresh look for HotCarbon",
+        "excerpt":"We are happy to present the redesigned HotCarbon website.  You will find here all updates and info for the workshop’s upcoming edition(s) and the papers and talks from the previous editions.  ","categories": [],
+        "tags": [],
+        "url": "/2023/11/10/new-website.html",
+        "teaser": null
+      },{
+        "title": "HotCarbon'24 CfP is out!",
+        "excerpt":"HotCarbon will be back in 2024! Submission deadline is May 8, 2024.  Check out the Call for Papers for details.  ","categories": ["news"],
+        "tags": [],
+        "url": "/news/2023/11/13/cfpOut.html",
+        "teaser": null
+      },{
+        "title": "Important updates on remote participation",
+        "excerpt":"See the Policies section of the CfP for important updates on remote participation to HotCarbon’24.  ","categories": [],
+        "tags": [],
+        "url": "/2024/02/11/remoteparticipation.html",
+        "teaser": null
+      },{
+        "title": "[NEW] Registration deadline",
+        "excerpt":"There is now a paper registration deadline, set to May 5, 2024 (AoE).  ","categories": [],
+        "tags": [],
+        "url": "/2024/04/13/linkupAndRegistrationDeadline.html",
+        "teaser": null
+      },{
+        "title": "Change in location",
+        "excerpt":"The workshop location is changed from Santa Clara to Santa Cruz, CA.  It will be held in UCSC Haybarn.  ","categories": [],
+        "tags": [],
+        "url": "/2024/04/14/locationChange.html",
+        "teaser": null
+      },{
+        "title": "Registration is now open!",
+        "excerpt":"The workshop registration is now open.  You can find further details about attending the event on the corresponding page.  ","categories": [],
+        "tags": [],
+        "url": "/2024/06/12/registration.html",
+        "teaser": null
+      },{
+        "title": "The program in online",
+        "excerpt":"The workshop program is online, along with all the final papers and the videos of the virtual presentations. Check them out!  ","categories": [],
+        "tags": [],
+        "url": "/2024/07/07/program.html",
+        "teaser": null
+      },{
+        "title": "Wrap up of HotCarbon'24",
+        "excerpt":"The 2024 edition of HotCarbon was a vibrant success! Thank you to all authors and participants for the great interaction.  Stay tuned for the 2025 edition!  ","categories": [],
+        "tags": [],
+        "url": "/2024/07/11/wrapUp.html",
+        "teaser": null
+      },{
+        "title": "Get ready for HotCarbon'25!",
+        "excerpt":"After the success of its third edition, HotCarbon will be back in 2025 with a 1.5 day program! (or even 2 days, if necessary). Like in previous editions, the workshop will be collocated with OSDI and take place on July 10 and 11 in Cambridge, MA, USA. We are delighted...","categories": [],
+        "tags": [],
+        "url": "/2024/12/12/2025on.html",
+        "teaser": null
+      },{
+        "title": "CfP out & Venue confirmed!",
+        "excerpt":"The HotCarbon 2025 Call for Papers is out! Submission deadline is May 12, 2025.   The workshop will take place on July 10-11th 2025 at the Microsoft NERD center, Cambridge, MA, USA.  Check out the Attend page for more details.  ","categories": [],
+        "tags": [],
+        "url": "/2025/02/14/cfpOut.html",
+        "teaser": null
+      },{
+        "title": "Submission site open",
+        "excerpt":"The submission site for HotCarbon 2025 is now open!   hotcarbon25.hotcrp.com  ","categories": [],
+        "tags": [],
+        "url": "/2025/04/23/hotcrp.html",
+        "teaser": null
+      },{
+        "title": "Submission deadline extended",
+        "excerpt":"Following multiple requests, the HotCarbon’25 submission deadline is extended by one week, to May 19, 2025 (AoE)   hotcarbon25.hotcrp.com  ","categories": [],
+        "tags": [],
+        "url": "/2025/05/12/deadlineExtended.html",
+        "teaser": null
+      },{
+        "title": "Slight delay in decision notifications",
+        "excerpt":"There will be a slight delay in the paper decision notification. Our apologies. Hang on, it’s coming soon!  ","categories": [],
+        "tags": [],
+        "url": "/2025/06/16/notificationDelay.html",
+        "teaser": null
+      },{
+        "title": "Keynote from Daniel S. Berger",
+        "excerpt":"HotCarbon’25 is trilled to announce that its program will feature a keynote from Daniel S. Berger, from Microsoft Azure Systems Research.   Stay tuned for the detailed program and registration info (very) soon!  ","categories": [],
+        "tags": [],
+        "url": "/2025/06/27/keynote.html",
+        "teaser": null
+      },{
+        "title": "Registration is now open!",
+        "excerpt":"Please note! In-person participant must register before July 7th to be allowed entry in the workshop building!   You can now register via eventbrite.  ","categories": [],
+        "tags": [],
+        "url": "/2025/06/29/registrationOpen.html",
+        "teaser": null
+      },{
+        "title": "Preliminary program is up!",
+        "excerpt":"The workshop program is now available!   HotCarbon’25 is trilled to announce that its program will feature a second keynote from Le Xie (Harvard) entitled The Interplay between AI and Electric Power Systems.  ","categories": [],
+        "tags": [],
+        "url": "/2025/07/01/program.html",
+        "teaser": null
+      },{
+        "title": "Wrap up of HotCarbon'25",
+        "excerpt":"HotCarbon’25 was a vibrant success! Thank you to all authors and participants for the great interaction.  You can find all the workshop’s papers and some of the authors’ presentations on the 2025 page.   Stay tuned for the 2026 edition!  ","categories": [],
+        "tags": [],
+        "url": "/2025/08/14/wrapUp.html",
+        "teaser": null
+      }]
